7238bdd6d3728d705fd834f6efafde0785cb16ce
AnnaSaurina <the_happy_ann@mail.ru>
2023-03-07 01:18:25 +0300

    Added function

---
+++ js/comment.js
@@ -0,0 +1,11 @@
+function published(){
+    event.preventDefault();
+
+    let userName=document.getElementById('name');
+    let commentText=document.getElementById('comment');
+
+    let comment={
+        
+    }
+
+}
