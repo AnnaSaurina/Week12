70f0bc2e69b4d8040b1c2199cbe83a46646b88a6
AnnaSaurina <the_happy_ann@mail.ru>
2023-05-01 23:35:54 +0300

    Saved

--- js/comment.js
+++ js/comment.js
@@ -4,23 +4,37 @@ let comment = document.getElementById('commentElement');
 let button = document.querySelector('.buttonElement');
 let chat = document.querySelector('.chat');
 
+function checkedWord(word) {
+    return (`${word.slice(0, 1).toUpperCase()}${word.slice(1).toLowerCase()}`);
+};
+
+function checkSpam(str)
+
+checkedWord('dfhfgLJK:h');
+
 button.addEventListener('click', () => {
-    console.log(button.value);
-    chat.innerHTML = '';
+    console.log(userName.value);
+
+    let urlAvatar = urlImage.value;
+    console.log(urlImage.value);
+
+    let user = userName.value;
+    let fullName = user.split(' ');
+    let surname = checkedWord(fullName[0]);
+    let yourName = checkedWord(fullName[1]); 
+    let patronymik = checkedWord(fullName[2]);
+
+    console.log(yourName);
+
+    let finalFullName = `${surname} ${yourName} ${patronymik}`;
+
+    chat.innerHTML = `
+        <div id="message" class="message">
+                    <p>Чат</p>
+                    <img src=${urlAvatar}
+                    alt="Аватар" class="avatar" />
+                    <p class="userName">${finalFullName}</p> 
+                    <p class="text">${comment.value}</p>
+                </div>
+                `;
 });
-
-
-
-
-
-// function published(){
-//     event.preventDefault();
-
-//     let userName=document.getElementById('name');
-//     let commentText=document.getElementById('comment');
-
-//     let comment={
- 
-//     }
-
-// }
