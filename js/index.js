89522d2b0865114c6338ab8c4ce3860e61a513d9
AnnaSaurina <the_happy_ann@mail.ru>
2023-05-16 18:11:17 +0300

    Saved

--- js/index.js
+++ js/index.js
@@ -3,12 +3,11 @@ let urlImage = document.getElementById('urlElement');
 let comment = document.getElementById('commentElement');
 let button = document.querySelector('.buttonElement');
 let chat = document.querySelector('.chat');
-
+let chatSpam = chat.replace(/xxx/gi, '***').replace(/viagra/gi, '***');
 
 function checkedWord(word) {
-    return (`${word.slice(0, 1).toUpperCase().slice(1, -1).toLowerCase()}`);
+    return (`${word.slice(0, 1).toUpperCase()}${word.slice(1,).toLowerCase()}`);
 };
-
 
 
 button.addEventListener('click', () => {
@@ -19,12 +18,10 @@ button.addEventListener('click', () => {
 
     let user = userName.value;
     let fullName = user.split(' ');
-    let finalFullName = checkedWord(fullName);
-    // let surname = checkedWord(fullName[0]);
-    // let yourName = checkedWord(fullName[1]); 
-    // let patronymik = checkedWord(fullName[2]);
-    // let finalFullName = `${surname} ${yourName} ${patronymik}`;
-    // let chatSpam = chat.replace(/xxx/gi, '***').replace(/viagra/gi, '***');
+    let surname = checkedWord(fullName[0]);
+    let yourName = checkedWord(fullName[1]); 
+    let patronymik = checkedWord(fullName[2]);
+    let finalFullName = `${surname} ${yourName} ${patronymik}`;
 
     chat.innerHTML = `
         <div id="message" class="message">
@@ -32,7 +29,7 @@ button.addEventListener('click', () => {
                     <img src=${urlAvatar}
                     alt="Аватар" class="avatar" />
                     <p class="userName">${finalFullName}</p> 
-                    <p class="text">${chat.value}</p>
+                    <p class="text">${chatSpam.value}</p>
                 </div>
                 `;
 });
