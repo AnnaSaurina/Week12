34eaf83a2f3a896aa86234146c4c3178fa7ef4b7
AnnaSaurina <the_happy_ann@mail.ru>
2023-05-25 17:45:26 +0300

    Saved function

--- js/index.js
+++ js/index.js
@@ -4,27 +4,15 @@ let comment = document.getElementById('commentElement');
 let button = document.querySelector('.buttonElement');
 let chat = document.querySelector('.chat');
 
-// Тестрирование replace
-
-// let textTest = 'XXX adasdasd Viagra sdfsdfsef xxxxxx'
-// let antiSpamText = textTest.replace(/xxx/gi, '***').replace(/viagra/gi, '***');
-// console.log(antiSpamText);
-
 function checkedWord(word) {
-    return (`${word.slice(0, 1).toUpperCase()}${word.slice(1,).toLowerCase()}`);
+    return (word.split(/\s+/).map(word => word[0].toUpperCase() + word.substring(1)).join(' '));
 };
 
 button.addEventListener('click', () => {
-    // console.log(userName.value);
 
     let urlAvatar = urlImage.value;
-    // console.log(urlImage.value);
     let user = userName.value;
-    let fullName = user.split(' ');
-    let surname = checkedWord(fullName[0]);
-    let yourName = checkedWord(fullName[1]);
-    let patronymik = checkedWord(fullName[2]);
-    let finalFullName = `${surname} ${yourName} ${patronymik}`;
+    let finalFullName = checkedWord(user);
     let commentChat = comment.value;
     let chatSpam = commentChat.replace(/xxx/gi, '***').replace(/viagra/gi, '***');
 
